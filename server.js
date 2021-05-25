20cd37cc9c91e749282179fa3455055e9f4d69ad
ada-beep <chichieva33@gmail.com>
2021-05-25 13:49:48 +0000

    nation-books

--- server.js
+++ server.js
@@ -14,16 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -41,4 +32,15 @@ app.use((error, req, res, next) => {
   res.status(error.status || 500);
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+  
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(PORT, () => console.log("Covering the nations book hole...."));
